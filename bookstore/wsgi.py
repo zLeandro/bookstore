fbb9cc41dd2f3341fd5071ed1064623231821638
Leandro Walter <leandromarqueswalter@gmail.com>
2025-01-31 13:56:21 -0300

    Corrige caminho do projeto no wsgi.py para refletir a estrutura de pastas

--- bookstore/wsgi.py
+++ bookstore/wsgi.py
@@ -7,10 +7,13 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
+import sys
 import os
+
+sys.path.append('/home/zLe/bookstore')
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookstore.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookstore.bookstore.settings")
 
 application = get_wsgi_application()
