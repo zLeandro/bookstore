e6e58856ab894a1f0a3a80aeb60a9ec97662e168
Leandro Walter <leandromarqueswalter@gmail.com>
2025-01-31 14:15:06 -0300

    atualiza repositorio

--- bookstore/wsgi.py
+++ bookstore/wsgi.py
@@ -12,6 +12,9 @@ import os
 
 sys.path.append('/home/zLe/bookstore')
 
+activate_this = '/home/zLe/env/bin/activate_this.py'
+exec(open(activate_this).read(), {'__file__': activate_this})
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "bookstore.bookstore.settings")
