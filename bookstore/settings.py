9de5968dda8d8c9088b08a967778ea75e2fc82f9
Leandro Walter <leandromarqueswalter@gmail.com>
2025-02-10 20:34:21 -0300

    atualiza repositorio

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+from decouple import Config, Csv
+
+config = Config()
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 # Application definition
