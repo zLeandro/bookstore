b321ac55c01b6042ae8ee8940ad5153f85d49f52
Leandro Walter <leandromarqueswalter@gmail.com>
2025-02-27 16:48:33 -0300

    atualiza repositorio

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from decouple import config
 
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Quick-start development settings - unsuitable for production
