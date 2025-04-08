aa83c6a04011ab1984d8a9d9c78c48a0fa98c459
Leandro Walter <leandromarqueswalter@gmail.com>
2025-04-08 14:56:25 -0300

    configura arquivos estáticos e adiciona staticfiles

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -76,7 +76,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "bookstore.wsgi.application"
 
-STATIC_ROOT = BASE_DIR / '/staticfiles'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Database
@@ -118,7 +118,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
