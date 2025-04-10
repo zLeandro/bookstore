32a79cc8249695dd76dc01f3b6753fd1e2c2509f
Leandro Walter <leandromarqueswalter@gmail.com>
2025-04-10 14:01:21 -0300

    Atualizando views

--- bookstore/views.py
+++ bookstore/views.py
@@ -1,10 +1,10 @@
 from django.views.decorators.csrf import csrf_exempt
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.core.management import call_command
 
 @csrf_exempt
 def update_server(request):
     if request.method == "POST":
-        return HttpResponse("Deploy manual não é necessário no Render.", status=200)
-    else:
-        return render(request, 'hello_world.html')
+        call_command("migrate")
+        return HttpResponse("Migração feita com sucesso no Render!", status=200)
+    return HttpResponse("Deploy manual não é necessário no Render.", status=200)
