ae91c8f92b2d364275c215f89bf433e4c94cb10d
Leandro Walter <leandromarqueswalter@gmail.com>
2025-04-10 14:14:50 -0300

    Atualizando views

--- bookstore/views.py
+++ bookstore/views.py
@@ -5,6 +5,5 @@ from django.core.management import call_command
 @csrf_exempt
 def update_server(request):
     if request.method == "POST":
-        call_command("migrate")
-        return HttpResponse("Migração feita com sucesso no Render!", status=200)
-    return HttpResponse("Deploy manual não é necessário no Render.", status=200)
+        return HttpResponse("Servidor alcançado. Tudo certo até aqui!", status=200)
+    return HttpResponse("GET não é usado aqui.", status=200)
